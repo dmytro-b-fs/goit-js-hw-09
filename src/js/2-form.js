61ae5213d689affe526c05da8c0b641e3e55b338
dmytro-b-fs <dmytro.burakov@gmail.com>
2026-05-31 13:21:21 +0300

    hw

---
+++ src/js/2-form.js
@@ -0,0 +1,27 @@
+const formEl = document.querySelector("form")
+
+const formData = {
+    email: "",
+    message: "",
+}
+
+formEl.addEventListener("input", (event)=>{
+    formData.email = formEl.email.value
+    formData.message = formEl.message.value
+
+    localStorage.setItem("feedback-form-state", JSON.stringify(formData))
+})
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    formEl.email.value = JSON.parse(localStorage.getItem("feedback-form-state")).email
+    formEl.message.value = JSON.parse(localStorage.getItem("feedback-form-state")).message
+})
+
+formEl.addEventListener("submit", (event)=>{
+    event.preventDefault()
+    if (formEl.email.value === "" || formEl.message.value === "") { alert("Fill please all fields")}
+    else{
+        console.log(localStorage.getItem("feedback-form-state"))
+        formEl.reset()
+    }
+})
